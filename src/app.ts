c138828fca465097cf167482c8816617cea11d20
muhammad rabiu <muhammadrabiu20040704@gmail.com>
2026-07-23 07:58:51 -0700

    chore: initialize MBK backend  with typescript and Express

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { healthCheck } from "controllers/health.controller.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/", healthCheck);
+
+export default app;
---
+++ src/controllers/health.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const healthCheck = (
+  req: Request,
+  res: Response
+): void => {
+  res.status(200).json({
+    success: true,
+    message: "MBK Backend is running 🚀",
+
+    version: "1.0.0",
+
+    environment: process.env.NODE_ENV,
+
+    timestamp: new Date().toISOString()
+  });
+};
